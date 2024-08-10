a76882bcf25356a63bee96b1bccbff1c2b82243b
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2024-08-10 23:07:48 +0530

    change section label

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Past Papers',
+            label: 'Resources',
           },
           { to: '/blog', label: 'Blog', position: 'left' },
           {
